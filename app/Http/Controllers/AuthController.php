2820fb4dcd23bcc519ecb623c44948db03bf65c5
Hoà Đoàn Duy <70DCTT21020@utteduvn.onmicrosoft.com>
2022-05-16 02:00:43 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,117 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|string|email|max:100',
+            'password' => 'required|string|min:6',
+        ]);
+        
+        $data = [
+            'email' =>  $request->email,
+            'password' => $request->password
+        ];
+
+        if (! $token = auth()->attempt($data)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return response()->json([
+            'message' => 'Login successfully',
+            'user' =>   auth()->user(),
+            'token' => $this->createNewToken($token)->original
+        ]);
+    }
+    /**
+     * Register a User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required|string|between:2,100',
+            'email' => 'required|string|email|max:100|unique:users,email,'.$request->email,
+            'password' => 'required|string|min:6',
+        ]);
+      
+        $data = [
+            'name' => $request->name,
+            'email' =>  $request->email,
+            'password' => $request->password
+        ];
+
+        $user = User::create(array_merge(
+                    $data,
+                    ['password' => bcrypt($request->password)]
+                ));
+        $token = auth()->attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+        return response()->json([
+            'message' => 'User successfully registered',
+            'user' => $user,
+            'token' => $this->createNewToken($token)->original
+        ], 201);
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout() {
+        auth()->logout();
+        return response()->json(['message' => 'User successfully signed out']);
+    }
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh() {
+        return $this->createNewToken(auth()->refresh());
+    }
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function userProfile() {
+        return response()->json(auth()->user());
+    }
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function createNewToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            // 'expires_in' => auth()->factory()->getTTL() * 60,
+            // 'user' => auth()->user()
+        ]);
+    }
+}
